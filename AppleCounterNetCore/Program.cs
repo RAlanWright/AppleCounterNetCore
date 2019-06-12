2112837c192f4e864b81807247906303309da29b
RAlanWright <wright.alan88@gmail.com>
2019-06-11 21:46:50 -0500

    Fixed exception handling

--- AppleCounterNetCore/Program.cs
+++ AppleCounterNetCore/Program.cs
@@ -11,31 +11,31 @@ namespace AppleCounterNetCore
                 return apples * price;
             }
         }
-        static void Main(string[] args)
-        {
-            // Command line dotnet application
-            Apple cost = new Apple();
-            
-            double total = cost.Calc(Convert.ToInt32(args[0]), .25);
-           
-                try
+    static void Main(string[] args)
+    {
+        // Command line dotnet application
+        Apple cost = new Apple();
+        
+       try
+            {
+                double total = cost.Calc(Convert.ToInt32(args[0]), .25);
+
+                if (args.Length > 0 && args.Length < 2)
                 {
-                    if (args.Length > 0 && args.Length < 2)
-                    {
-                        Console.WriteLine($"Cost of {args[0]} apple(s): ${total}");
-                        Console.WriteLine("Press Enter to exit");
-                        Console.ReadLine();
-                    }
-                    else if (args.Length > 1)
-                    {
-                        Console.WriteLine("Please use one argument only. \nUsage: dotnet run 5");
-                        
-                    }
+                    Console.WriteLine($"Cost of {args[0]} apple(s): ${total}");
+                    Console.WriteLine("Press Enter to exit");
+                    Console.ReadLine();
                 }
-                catch (IndexOutOfRangeException e)
+                else if (args.Length > 1)
                 {
-                    throw new ArgumentOutOfRangeException("You must use one argument.\n Usage: dotnet run 5", e);
+                    Console.WriteLine("Please use one argument only. \nUsage: dotnet run 5");
                 }
-            }            
-        }
+            }
+            catch (IndexOutOfRangeException stopHere)
+            {
+                Console.WriteLine(stopHere.Message);
+                throw new System.ArgumentOutOfRangeException("\n\nYou must use one argument.\nUsage: dotnet run 5\n\n", stopHere);
+            }
+        }            
     }
+}
